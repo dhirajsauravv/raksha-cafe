acdf4b85b13d089214d67d77dee1a79b8de94fcd
Dhiraj Saurav <sauravdhiraj83@gmail.com>
2026-07-13 01:12:43 +0530

    add note in order summary

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import useCart from "../services/useCart";
 import { useNavigate } from "react-router";
 import useAuth from "../services/useAuth";
 import { MdCoffeeMaker } from "react-icons/md";
+import { FaCoffee } from "react-icons/fa";
 
 function Cart() {
   const { cart } = useCart();
@@ -41,7 +42,7 @@ function Cart() {
                 ))}
               </div>
             </div>
-            <div className=" bg-amber-100 rounded-3xl w-2/6 h-100 flex flex-col gap-4 m-10">
+            <div className=" bg-amber-100 rounded-3xl w-2/6 h-150 flex flex-col gap-4 m-10">
               <h1 className="text-center text-amber-700 font-bold text-3xl py-4">
                 Order Summary
               </h1>
@@ -50,12 +51,22 @@ function Cart() {
               <h1 className="font-bold text-xl pl-4">GST(5%): ₹{gst}</h1>
               <hr />
               <h2 className="font-bold text-xl pl-4">Total: ₹{total}</h2>
-              <div className="flex justify-center">
+              <hr />
+              <div className="pl-4">
+                <p className="font-bold text-xl">Add Note:</p>
+                <input
+                  type="text"
+                  placeholder="Enter Note"
+                  className="bg-white border field-sizing-fixed  text"
+                />
+              </div>
+
+              <div className="flex flex-col justify-center items-center">
                 <button
                   onClick={handleCheckout}
-                  className="bg-amber-300 text-amber-700 font-bold rounded-4xl p-5 hover:bg-amber-300/90 hover:scale-105"
+                  className="bg-amber-300 text-amber-700 font-bold rounded-full p-5 hover:bg-amber-300/90 hover:scale-105 text-2xl"
                 >
-                  Checkout
+                  Start Brewing
                 </button>
               </div>
             </div>
@@ -66,7 +77,7 @@ function Cart() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center text-8xl  bg-amber-100">
+    <div className="min-h-screen flex items-center justify-center text-8xl  bg-white">
       <div className="flex flex-col justify-center items-center gap-4 bg-amber-200 p-10 rounded-4xl">
         <p className="text-amber-600">Start Brewing</p>
         <MdCoffeeMaker size={150} className="text-amber-600" />
